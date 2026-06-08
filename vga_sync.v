3f8919739940bdf39e311629158be40a6d7ca09d
lance994 <159098557+lance994@users.noreply.github.com>
2026-06-07 17:24:34 -0700

    final vga_sync

--- vga_sync.v
+++ vga_sync.v
@@ -28,7 +28,6 @@ module vga_sync
 		output wire [9:0] x, y
 	);
 	
-	// constant declarations for VGA sync parameters
 	localparam H_DISPLAY       = 640; // horizontal display area
 	localparam H_L_BORDER      =  48; // horizontal left border
 	localparam H_R_BORDER      =  16; // horizontal right border
@@ -45,7 +44,6 @@ module vga_sync
     localparam START_V_RETRACE = V_DISPLAY + V_B_BORDER;
 	localparam END_V_RETRACE   = V_DISPLAY + V_B_BORDER + V_RETRACE - 1;
 	
-	// mod-4 counter to generate 25 MHz pixel tick
 	reg [1:0] pixel_reg;
 	wire [1:0] pixel_next;
 	wire pixel_tick;
@@ -56,18 +54,15 @@ module vga_sync
 		else
 		  pixel_reg <= pixel_next;
 	
-	assign pixel_next = pixel_reg + 1; // increment pixel_reg 
+	assign pixel_next = pixel_reg + 1; 
 	
-	assign pixel_tick = (pixel_reg == 0); // assert tick 1/4 of the time
+	assign pixel_tick = (pixel_reg == 0); 
 	
-	// registers to keep track of current pixel location
 	reg [9:0] h_count_reg, h_count_next, v_count_reg, v_count_next;
 	
-	// register to keep track of vsync and hsync signal states
 	reg vsync_reg, hsync_reg;
 	wire vsync_next, hsync_next;
  
-	// infer registers
 	always @(posedge clk, posedge reset)
 		if(reset)
 		    begin
@@ -84,7 +79,6 @@ module vga_sync
                     hsync_reg   <= hsync_next;
 		    end
 			
-	// next-state logic of horizontal vertical sync counters
 	always @*
 		begin
 		h_count_next = pixel_tick ? 
@@ -96,20 +90,15 @@ module vga_sync
 			       : v_count_reg;
 		end
 		
-        // hsync and vsync are active low signals
-        // hsync signal asserted during horizontal retrace
         assign hsync_next = h_count_reg >= START_H_RETRACE
                             && h_count_reg <= END_H_RETRACE;
    
-        // vsync signal asserted during vertical retrace
         assign vsync_next = v_count_reg >= START_V_RETRACE 
                             && v_count_reg <= END_V_RETRACE;
 
-        // video only on when pixels are in both horizontal and vertical display region
         assign video_on = (h_count_reg < H_DISPLAY) 
                           && (v_count_reg < V_DISPLAY);
 
-        // output signals
         assign hsync  = ~hsync_reg;
         assign vsync  = ~vsync_reg;
         assign x = h_count_reg;
